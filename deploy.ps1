ac208a10abe338ad5b88fad889b210980c15a48d
brendo <admin@brendanmorrel.com>
2019-08-06 19:27:43 +0800

    deploy

--- deploy.ps1
+++ deploy.ps1
@@ -1,8 +1,20 @@
 #! /usr/bin/pwsh
 
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+$PSDefaultParameterValues['*:ErrorAction']='Stop'
+
 yarn test --no-watch
 
+if ($lastexitcode -ne 0) {
+  throw ("Exec error")
+}
+
 yarn build
+
+if ($lastexitcode -ne 0) {
+  throw ("Exec error")
+}
 
 $version = (Get-Content package.json) -join "`n" | ConvertFrom-Json | Select -ExpandProperty "version"
 if ($version -like '*rc*') { 
