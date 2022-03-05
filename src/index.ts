ead3ac99367c8b02bebee958237799bb24d2704f
brendo <you@example.com>
2022-03-05 08:17:25 +0800

    v3.3.2

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,5 @@ export type { IHub } from "./IHub";
 export { createHub, addGlobalGeneratorMiddleware } from "./hub";
 export type { IGeneratorBuilder } from "./generatorBuilder";
 export type { EventData, EventMiddleware, EventMiddlewareContext } from "./types";
-
 export type { EventIterable, EventFunction } from "./generator";
 export { take, put, call, fork, delay, takeLatest, takeEvery, takeLast } from "./generator";
